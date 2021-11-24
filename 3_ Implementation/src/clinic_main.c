8929fd8248b84ce8426cc571be0bbc8054849963
PAVITHRARAVICHANDRAN03 <94224532+PAVITHRARAVICHANDRAN03@users.noreply.github.com>
2021-11-24 15:45:34 +0530

    Update clinic_main.c

--- 3_ Implementation/src/clinic_main.c
+++ 3_ Implementation/src/clinic_main.c
@@ -1,111 +1,92 @@
-
-#include <stdio.h>
-#include <stdlib.h>
-#include "STD_Types.h"
-#include "header.h"
-
-node *head=NULL;
-node *current = NULL;
-node *prev = NULL;
-u32 ListLength = 0,Size=0,ID,time,i;
-u8 name[100];
-u8 gender[10];
-u32 pat_age,pat_id,id;
-void main(void)
+#include<stdio.h>
+#include<string.h>
+struct ad
 {
-	u32 choice,input,newValue,Node_Value,position,PW;
-	u8   flag=0,Exit_flag=1;
-		
-	while(Exit_flag)
-	{
-	printf("Enter 1 for admin mode (or) enter 2 for user mode (or) enter 3 to exit\n");
-	printf("Your choice: ");
-	scanf("%d",&choice);
-	if(choice==1)
-	{
-	printf("\t\t\t******** Admin Mode ********\n\n");
-	 printf("Please Enter the Password: ");
-		for (i=0; i<3; i++)
-		{
-			  scanf ("%d",&PW);
-					   if(PW==1234)
-					   {
-						   printf("\t\t\t******** Welcome ********\n\n");
-						   flag = 1;
-						   break;
-					   }
-					   
-					   else if (i<2)
-					   {
-						   printf("Try again: ");
-					   }
-					   
-	   }
-	   if(flag==1)
-	   {
-		printf("if you want to add new patient record press 1\n");
-		printf("if you want to edit patient record press 2\n");
-		printf("if you want to reserve a slot with the doctor press 3\n");
-		printf("if you want to cancel reservation press 4\n");
-		printf("Your Choice: ");
-		scanf("%d",&input);
-		switch (input)
-		{
-			case 1 : printf("Please Enter the patient's name: ");
-			         scanf(" %[^\n]%*c",name);
-					 printf("Please Enter the patient's age: ");
-			         scanf("%d",&pat_age);
-					 printf("Please Enter the patient's gender (male or female): ");
-			         scanf(" %[^\n]%*c",gender);
-					 printf("Please Enter the patient's id: ");
-			         scanf("%d",&pat_id);
-					 Add_patient(name,pat_age,gender,pat_id);
-					 break;
-			case 2 : printf("Enter Your ID: ");
-					 scanf("%d",&id);
-					Edit_List(id); break;
-			case 3 : Time_Reservation();
-					 break;
-			case 4 : printf("Enter Your ID: ");
-					 scanf("%d",&id);
-			         Cancel_Reservation(id);
-					 break;
-			default: printf("Invalid Choice please try again\n"); break;
-		}
-	   }
-		else if (flag == 0)
-		{
-		printf("Incorrect password for 3 consecutive times\n"); Exit_flag=0;
-		}
-	}
-	
-	else if(choice==2)
-	{
-		printf("\t\t\t******** User Mode ********\n\n");
-		printf("For Patient record press 1\n");
-		printf("For today's reservation press 2\n");
-		printf("Your choice: ");
-		scanf("%d",&choice);
-		switch (choice)
-		{
-			case 1 : printf("Enter Your ID: ");
-					 scanf("%d",&id);
-					 printf("\t\t\t********** Patient Record **********\n\n");
-					 Print_List(id);
-					 break;
-			case 2 : printf("\t\t\t****** Today's Reservation ******\n\n ");
-					Print_Res();
-					break;
-			default: printf("Invalid Choice please try again\n"); break;
-		}
-	}
-	else if(choice==3)
-		
-		{
-		Exit_flag=0; 
-		printf("Good Bye");
-		}
-	else 
-		printf("Wrong entry\n");
-	}
+    char name[30];
+    char disease[30];
+    int cabin,phone,age;
+} x[100];
+int n,i,j=0,a=0,sum=0,g,flag,num;
+void read();
+void add();
+void view();
+void search();
+void edit();
+void del();
+void show();
+int main()
+{
+    read();
+    int c,i,q;
+    printf("Simple Clinic Management System\n");
+    int m,n;
+//making out the pattern
+
+    for(m=1; m<=4; m++)
+    {
+        for(n=1; n<=5; n++)
+            printf(" ");
+        for(n=1; n<=m-1; n++)
+        {
+            printf(" ");
+        }
+        for(n=1; n<=4-m+1; n++)
+        {
+            if(n==4-m+1 || m==1 || m==4)
+                printf("*");
+            else
+                printf(" ");
+        }
+        for(n=1; n<=4-m+1; n++)
+        {
+            if(n==1 ||m==1 || m==4)
+                printf("*");
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+    while(c!=6)
+    {
+
+        printf("**Enter your choice**\n\n1. Add Information\n2. View Inform
+        scanf("%d",&c);//choice for option
+        fflush(stdin);//making it clear
+        if(c==1)//add
+        {
+            system("cls");
+            add();
+        }
+        else if(c==2)//view
+        {
+            system("cls");
+            view();
+        }
+        else if(c==3)//search
+        {
+            system("cls");
+            search();
+        }
+        else if(c==4)//edit
+        {
+            system("cls");
+            edit();
+        }
+        else if(c==5)//delete
+        {
+            system("cls");
+            del();
+        }
+        else if(c==6)
+        {
+            write();
+            return 0;
+        }
+        else
+        {
+            system("cls");
+            printf("\n\nInvalid input , try again by using valid inputs");
+        }
+        printf("\n\n");
+    }
 }
