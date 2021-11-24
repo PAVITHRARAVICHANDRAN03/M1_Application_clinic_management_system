55a931017a6956cb6944edb385faca2fc17d2d4b
PAVITHRARAVICHANDRAN03 <94224532+PAVITHRARAVICHANDRAN03@users.noreply.github.com>
2021-11-24 15:37:34 +0530

    Update and rename SDT_Type.h to Clinic_system.h

---
+++ 3_ Implementation/inc/Clinic_system.h
@@ -0,0 +1,12 @@
+#ifndef CLINIC_C /* IF NOT DEFINE THE CLINIC_H*/
+#define CLINIC_C /* DEFINING THE CLINIC_H*/
+// Structure that stores all details of a patient
+typedef struct patient{
+    char name;
+    int age;
+    int phone;
+    int cabin;
+    char disease;
+   
+}patient_details;
+#endif
